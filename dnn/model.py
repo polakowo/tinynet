a9ccb4682bdf1663a36bb4ab006e33a0001831df
Oleg Polakow <olegpolakow@gmail.com>
2019-09-05 18:29:47 +0200

    Update documentation

--- dnn/model.py
+++ dnn/model.py
@@ -15,16 +15,12 @@ class Sequential:
         self.layers = []
 
     def add(self, layer):
+        """Add a new layer to the NN."""
         self.layers.append(layer)
 
-    ###########
-    # COMPILE #
-    ###########
 
     def configure(self, in_shape, optimizer, cost_fn, regularizer=None):
-        """
-        Initialize layer and optimization params
-        """
+        """Initialize the layers and the optimization params."""
         in_shape = (None, *in_shape[1:])
         self.optimizer = optimizer
         self.cost_fn = cost_fn
@@ -43,10 +39,9 @@ class Sequential:
         elif isinstance(self.optimizer, optimizers.Adam):
             self.optimizer.init_params(self.layers)
 
+
     def summary(self):
-        """
-        Get summary on layer shapes and parameters
-        """
+        """Print the summary on layer shapes and parameters."""
         rows = []
         for layer in self.layers:
             name = layer.__class__.__name__
@@ -58,22 +53,18 @@ class Sequential:
             rows.append((name, out_shape, num_params))
         print(tabulate(rows, headers=['Layer class', 'Output shape', 'Params']))
 
-    #######################
-    # FORWARD PROPAGATION #
-    #######################
 
     def propagate_forward(self, X, predict=False):
+        """Propagate through the layers forwards."""
         out = X
         for l, layer in enumerate(self.layers):
             out = layer.forward(out, predict=predict)
 
         return out
 
-    ########
-    # COST #
-    ########
 
     def compute_cost(self, out, Y, epsilon=1e-12):
+        """Compute the cost."""
         m = out.shape[0]
 
         cost = self.cost_fn(out, Y, delta=False)
@@ -84,11 +75,9 @@ class Sequential:
 
         return cost
 
-    ########################
-    # BACKWARD PROPAGATION #
-    ########################
 
     def propagate_backward(self, out, Y):
+        """Propagate through the layers backwards."""
         dX = self.cost_fn(out, Y, delta=True)
 
         # Calculate and store gradients in each layer with parameters
@@ -99,11 +88,9 @@ class Sequential:
             else:
                 dX = layer.backward(dX)
 
-    #################
-    # UPDATE PARAMS #
-    #################
 
     def update_params(self):
+        """Update the model parameters based on the optimization method."""
         if isinstance(self.optimizer, optimizers.GradientDescent):
             self.optimizer.update_params(self.layers, regularizer=self.regularizer)
         if isinstance(self.optimizer, optimizers.Momentum):
@@ -111,11 +98,9 @@ class Sequential:
         elif isinstance(self.optimizer, optimizers.Adam):
             self.optimizer.update_params(self.layers, regularizer=self.regularizer)
 
-    #########
-    # TRAIN #
-    #########
 
     def generate_batches(self, X, Y, batch_size, rng=None):
+        """Divide the dataset into batches."""
         if rng is None:
             rng = np.random
         m = X.shape[0]
@@ -137,10 +122,7 @@ class Sequential:
         return batches
 
     def fit(self, X, Y, nb_epoch, batch_size=None):
-        """
-        Train an L-layer neural network
-        """
-
+        """Train a multi-layer neural network."""
         costs = []
         # Progress information is displayed and updated dynamically in the console
         batches = self.generate_batches(X, Y, batch_size)
@@ -149,7 +131,6 @@ class Sequential:
             for epoch in range(nb_epoch):
                 # Diversify outputs by epoch but make them predictable
                 rng = np.random.RandomState(epoch)
-
                 if batch_size is not None:
                     # Divide the dataset into mini-batches based on their size
                     # We increment the seed to reshuffle differently the dataset after each epoch
@@ -157,32 +138,23 @@ class Sequential:
                 else:
                     # Batch gradient descent
                     batches = [(X, Y)]
-
                 for i, batch in enumerate(batches):
                     # Unpack the mini-batch
                     X_batch, Y_batch = batch
-
                     # Forward propagation
                     out = self.propagate_forward(X_batch)
-
                     # Compute cost
                     cost = self.compute_cost(out, Y_batch)
                     costs.append(cost)
-
                     # Backward propagation
                     self.propagate_backward(out, Y_batch)
-
                     # Update params with an optimizer
                     self.update_params()
-
                     pbar.update(1)
 
         return costs
 
-    ###########
-    # PREDICT #
-    ###########
 
     def predict(self, X):
-        # Propagate forward with the parameters learned previously
+        """Propagate forward with the parameters learned previously."""
         return self.propagate_forward(X, predict=True)
